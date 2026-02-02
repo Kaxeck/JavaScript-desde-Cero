1a92696ce8e23426b7832e3b4627d029caf818c3
Keith <keithalexis581@gmail.com>
2026-02-02 15:05:09 -0600

    Se agrega ejercicio de tipos de datos

---
+++ Proyecto 1 Introducción a JavaScript/tipos-de-dato.js
@@ -0,0 +1,24 @@
+// Tipos datos
+// Ejemplos de cada tipo de dato
+
+// Strings
+console.log(typeof "Esto es un string");
+console.log(typeof "Veinticinco");
+console.log(typeof "");
+
+// Numbers
+console.log(typeof 42);
+console.log(typeof -666);
+console.log(typeof 0);
+console.log(typeof 3.14);
+
+//Booleans
+console.log(typeof true);
+console.log(typeof false);
+
+// Null
+console.log(typeof null);
+
+// Undefined
+console.log(typeof undefined);
+console.log(typeof FALSE);
