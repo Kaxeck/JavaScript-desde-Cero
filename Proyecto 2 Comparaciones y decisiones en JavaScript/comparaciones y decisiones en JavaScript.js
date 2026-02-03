7067f68ee107933c2f78564fb90945ebe04d454f
Keith <keithalexis581@gmail.com>
2026-02-02 19:17:45 -0600

    Se agregan comentarios de la segunda practica

--- Proyecto 2 Comparaciones y decisiones en JavaScript/comparaciones y decisiones en JavaScript.js
+++ Proyecto 2 Comparaciones y decisiones en JavaScript/comparaciones y decisiones en JavaScript.js
@@ -1,14 +1,21 @@
+// Variable de la nota
 let nota = 78;
 
+// Estructura de decisiones para evaluar la nota
+
+// Si la nota es mayor o igual a 90 -> "Excelente"
 if (nota >= 90) {
     console.log("Excelente");
 }
+// Si la nota es entre 75 y 89 -> "Bien"
 else if (nota >= 75 && nota <= 89) {
     console.log("Bien");
 }
+// Si la nota es entre 60 y 74 -> "Suficiente"
 else if (nota >= 60 && nota <= 74) {
     console.log("Suficiente");
 }
+// Si la nota es menor a 60 -> "No aprobado"
 else {
     console.log("No aprobado");
 }
