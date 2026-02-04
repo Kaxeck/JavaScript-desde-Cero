8968b5ac408b41fb950f6e9f16a7276de58f7eb5
Keith <keithalexis581@gmail.com>
2026-02-03 18:56:09 -0600

    Se agregan mas pruebas a la practica 2

--- Proyecto 2 Comparaciones y decisiones en JavaScript/comparaciones y decisiones en JavaScript.js
+++ Proyecto 2 Comparaciones y decisiones en JavaScript/comparaciones y decisiones en JavaScript.js
@@ -1,21 +1,89 @@
+// Ejemplo nota 1 (Bien)
 // Variable de la nota
-let nota = 78;
+let nota1 = 78;
 
 // Estructura de decisiones para evaluar la nota
-
 // Si la nota es mayor o igual a 90 -> "Excelente"
-if (nota >= 90) {
+if (nota1 >= 90) {
     console.log("Excelente");
 }
 // Si la nota es entre 75 y 89 -> "Bien"
-else if (nota >= 75 && nota <= 89) {
+else if (nota1 >= 75 && nota1 <= 89) {
     console.log("Bien");
 }
 // Si la nota es entre 60 y 74 -> "Suficiente"
-else if (nota >= 60 && nota <= 74) {
+else if (nota1 >= 60 && nota1 <= 74) {
     console.log("Suficiente");
 }
 // Si la nota es menor a 60 -> "No aprobado"
+else {
+    console.log("No aprobado");
+}
+
+// Ejemplo nota 2 (Suficiente)
+// Variable de la nota
+let nota2 = 60;
+
+if (nota2 >= 90) {
+    console.log("Excelente");
+}
+else if (nota2 >= 75 && nota2 <= 89) {
+    console.log("Bien");
+}
+else if (nota2 >= 60 && nota2 <= 74) {
+    console.log("Suficiente");
+}
+else {
+    console.log("No aprobado");
+}
+
+// Ejemplo nota 3 (No aprobado)
+// Variable de la nota
+let nota3 = 45;
+
+if (nota3 >= 90) {
+    console.log("Excelente");
+}
+else if (nota3 >= 75 && nota3 <= 89) {
+    console.log("Bien");
+}
+else if (nota3 >= 60 && nota3 <= 74) {
+    console.log("Suficiente");
+}
+else {
+    console.log("No aprobado");
+}
+
+// Ejemplo nota 4 (Excelente)
+// Variable de la nota
+let nota4 = 90;
+
+if (nota4 >= 90) {
+    console.log("Excelente");
+}
+else if (nota4 >= 75 && nota4 <= 89) {
+    console.log("Bien");
+}
+else if (nota4 >= 60 && nota4 <= 74) {
+    console.log("Suficiente");
+}
+else {
+    console.log("No aprobado");
+}
+
+// Ejemplo nota 5 (Fuera de rango)
+// Variable de la nota
+let nota5 = 110;
+
+if (nota5 >= 90) {
+    console.log("Excelente");
+}
+else if (nota5 >= 75 && nota5 <= 89) {
+    console.log("Bien");
+}
+else if (nota5 >= 60 && nota5 <= 74) {
+    console.log("Suficiente");
+}
 else {
     console.log("No aprobado");
 }
